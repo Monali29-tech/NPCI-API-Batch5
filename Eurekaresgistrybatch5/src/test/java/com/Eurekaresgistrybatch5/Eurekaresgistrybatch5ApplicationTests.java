16f1767dbcb1dc9826fb7ee9b822fb3bd047a12a
admin <admin@DESKTOP-CI8HQV3>
2025-02-19 11:28:00 +0530

    first1.1

---
+++ Eurekaresgistrybatch5/src/test/java/com/Eurekaresgistrybatch5/Eurekaresgistrybatch5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Eurekaresgistrybatch5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eurekaresgistrybatch5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
